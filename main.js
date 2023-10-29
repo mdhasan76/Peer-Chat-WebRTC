2d00e052bcb1b59a9bdd588f0e97d563186ed2e1
Md Hasan Mia <you@example.com>
2023-10-29 12:34:43 +0600

    create offer and candidate and add agora appid

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let localStream;
 let peerConnection;
 let remoteStream;
+let APP_ID = "0ac5b4566af848feb8b4f4c5f8adafca";
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000));
+
+let client;
+let channel;
 
 const servers = {
   iceServers: [
@@ -11,16 +17,35 @@ const servers = {
 };
 
 let init = async () => {
+  client = await AgoraRTM.createInstance(APP_ID);
+  await client.login({ uid, token });
+
+  // index.html?rome=12345
+  channel = client.createChannel("main");
+  await channel.join();
+
+  channel.on("MemberJoined", handleUserJoined);
+
+  client.on("MessageFromPeer", handleMessageFromPeer);
+
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false,
   });
   document.getElementById("user-1").srcObject = localStream;
-
-  createOffer();
 };
 
-let createOffer = async () => {
+let handleUserJoined = async (memberId) => {
+  console.log("A new User joined the channel: ", memberId);
+  createOffer(memberId);
+};
+
+let handleMessageFromPeer = (message, memberId) => {
+  message = JSON.parse(message.text);
+  console.log("Message", message);
+};
+
+let createOffer = async (memberId) => {
   peerConnection = new RTCPeerConnection(servers);
 
   remoteStream = new MediaStream();
@@ -38,14 +63,26 @@ let createOffer = async () => {
 
   peerConnection.onicecandidate = async (event) => {
     if (event.candidate) {
-      console.log("New ICE Candidate: ", event.candidate);
+      client.sendMessageToPeer(
+        {
+          text: JSON.stringify({
+            type: "candidate",
+            candidate: event.candidate,
+          }),
+        },
+        memberId
+      );
     }
   };
 
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
-  console.log("offer: ", offer);
+  // console.log("offer: ", offer);
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "offer", offer: offer }) },
+    memberId
+  );
 };
 
 // console.log(document.getElementById("user-1"));
