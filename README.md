7d4bc818ef105a59849cd755416aeb008a11fe23
Md Hasan Mia <you@example.com>
2023-10-29 16:07:11 +0600

    create sdp answer

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 1st seen 23:13 sec
 2nd seen 35:00 sec
+3rd seen 50:00 min
+4th seen 1:02:03 min
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let remoteStream;
 let APP_ID = "0ac5b4566af848feb8b4f4c5f8adafca";
 let token = null;
 let uid = String(Math.floor(Math.random() * 10000));
-
+console.info("This is uid", uid);
 let client;
 let channel;
 
@@ -25,7 +25,7 @@ let init = async () => {
   await channel.join();
 
   channel.on("MemberJoined", handleUserJoined);
-
+  channel.on("MemberLeft", handleUserLeft);
   client.on("MessageFromPeer", handleMessageFromPeer);
 
   localStream = await navigator.mediaDevices.getUserMedia({
@@ -35,21 +35,51 @@ let init = async () => {
   document.getElementById("user-1").srcObject = localStream;
 };
 
+// New user Join handling function
 let handleUserJoined = async (memberId) => {
   console.log("A new User joined the channel: ", memberId);
   createOffer(memberId);
 };
 
+//  sending msg from one to another fn
 let handleMessageFromPeer = (message, memberId) => {
   message = JSON.parse(message.text);
-  console.log("Message", message);
+  if (message?.type === "offer") {
+    createAnswer(memberId, message?.offer);
+  }
+
+  if (message?.type === "answer") {
+    addAnswer(message?.answer);
+  }
+
+  if (message?.type === "candidate") {
+    if (peerConnection) {
+      peerConnection.addIceCandidate(message?.candidate);
+    }
+  }
+  // console.log("Message", message);
 };
 
-let createOffer = async (memberId) => {
+const handleUserLeft = async (memberId) => {
+  document.getElementById("user-2").style.display = "none";
+};
+
+// Create peer connection
+let createPeerConnection = async (memberId) => {
   peerConnection = new RTCPeerConnection(servers);
 
   remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+  document.getElementById("user-2").style.display = "block";
+
+  // extra checker for access camera and audio
+  if (!localStream) {
+    localStream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    });
+    document.getElementById("user-1").srcObject = localStream;
+  }
 
   localStream.getTracks().forEach((track) => {
     peerConnection.addTrack(track, localStream);
@@ -57,7 +87,7 @@ let createOffer = async (memberId) => {
 
   peerConnection.ontrack = (event) => {
     event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack();
+      remoteStream.addTrack(track);
     });
   };
 
@@ -74,6 +104,10 @@ let createOffer = async (memberId) => {
       );
     }
   };
+};
+
+let createOffer = async (memberId) => {
+  await createPeerConnection(memberId);
 
   let offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
@@ -85,5 +119,32 @@ let createOffer = async (memberId) => {
   );
 };
 
+let createAnswer = async (memberId, offer) => {
+  await createPeerConnection(memberId);
+
+  await peerConnection.setRemoteDescription(offer);
+  let answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  client.sendMessageToPeer(
+    { text: JSON.stringify({ type: "answer", answer: answer }) },
+    memberId
+  );
+};
+
+const addAnswer = async (answer) => {
+  if (!peerConnection.currentRemoteDescription) {
+    peerConnection.setRemoteDescription(answer);
+  }
+};
+
+const leaveChannel = async () => {
+  console.log("this is thw client who leave", client);
+  console.log("this is thw channel who leave", channel);
+  await channel.leave();
+  await client.logout();
+};
+
+window.addEventListener("beforeunload", leaveChannel);
 // console.log(document.getElementById("user-1"));
 init();
